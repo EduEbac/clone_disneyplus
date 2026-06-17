87ced59b8132be181916f067bf37c15addd600e9
Eduardo Figueiredo Basso <edu_fabric@outlook.com>
2026-06-17 20:37:20 -0300

    M21 Projeto Clone Disney+ : Fim da aula 8: compreender a estrutura e os componentes de uma seção de perguntas frequentes (FAQ); aprender a aplicar transições CSS para melhorar a experiência do usuário; praticar a criação de componentes reutilizáveis.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll("[data-tab-button]");
   const lists = document.querySelectorAll("[data-tab-id]");
   const STORAGE_KEY = "showsActiveTab";
+  const questions = document.querySelectorAll('[data-faq-question');
+
+  questions.forEach((quest) => quest.addEventListener('click', open_close_question));
+  
+  function open_close_question (element) {
+    const openViewQuest = 'faq__questions__item--is-open';
+    const elementFather = element.target.parentNode;
+    elementFather.classList.toggle(openViewQuest); // alterna true / false
+  }
 
   function activateTab(tabValue) {
     // buttons
@@ -32,11 +41,11 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-// Passos a passo:
-
 // Captura o clique no botão
 // Lê o valor de data-tab-button (ex: "populares")
 // Remove shows__tabs__button--is-active de todos os buttons
 // Adiciona a classe no button clicado
 // Remove shows__list--is-active de todas as <ul>
 // Adiciona a classe na <ul> que tem data-tab-id com o mesmo valor
+
+
